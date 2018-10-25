b0f1b031e591bac757a8f0f7d8935207bbcfeef5
lambrose <l.ambrose1@nuigalway.ie>
2018-10-25 20:54:51 +0100

    Q1 Complete

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main(int arg, char* argc[])
+{
+    //declaing different variable types
+    int integer1;
+    int* integerPointer;
+    long longInteger;
+    double* doublePointer;
+    char** characterDPointer;
+    
+    //finding the different byte sizes using sizeof
+    printf("Size of int: %ldß bytes\n",sizeof(integer1));
+    printf("Size of int*: %ld bytes\n",sizeof(integerPointer));
+    printf("Size of long: %ld bytes\n",sizeof(longInteger));
+    printf("Size of double*: %ld bytes\n",sizeof(doublePointer));
+    printf("Size of char**: %ld bytes\n",sizeof(characterDPointer));
 }
